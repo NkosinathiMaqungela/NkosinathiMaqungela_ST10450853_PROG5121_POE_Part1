da8f377cc6283df58b18b3124cfb08149dd3cbcb
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2026-04-14 10:25:19 +0200

    Added variables for each class

---
+++ src/nkosinathimaqungela_st10450853/prog5121_poe/Login.java
@@ -0,0 +1,15 @@
+
+package nkosinathimaqungela_st10450853.prog5121_poe;
+
+/**
+ *
+ * @author maqun
+ */
+public class Login {
+    private String username;
+    private String password;
+    private Registration registration;
+        private boolean loginStatus;
+
+    
+}
---
+++ src/nkosinathimaqungela_st10450853/prog5121_poe/Registration.java
@@ -0,0 +1,13 @@
+
+package nkosinathimaqungela_st10450853.prog5121_poe;
+
+public class Registration {
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+    private String firstName;
+    private String lastName;
+    private String storedUsername;
+    private String storedPassword;
+    
+}
