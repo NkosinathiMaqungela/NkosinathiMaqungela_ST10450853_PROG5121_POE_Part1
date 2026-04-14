21ca8bb9fdf924b85fd9c5f576ac993c13a2302f
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2026-04-14 10:29:46 +0200

    Added functionality that checks for the username and password

--- src/nkosinathimaqungela_st10450853/prog5121_poe/Registration.java
+++ src/nkosinathimaqungela_st10450853/prog5121_poe/Registration.java
@@ -1,13 +1,42 @@
 
 package nkosinathimaqungela_st10450853.prog5121_poe;
 
+import java.util.Scanner;
+
 public class Registration {
     private String username;
     private String password;
-    private String cellPhoneNumber;
+    private String cellNumber;
     private String firstName;
     private String lastName;
     private String storedUsername;
     private String storedPassword;
+    
+    Scanner scanner = new Scanner(System.in);
+
+    public Registration(String username, String password, String cellNumber
+            , String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.cellNumber = cellNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public boolean checkUserName() {
+        if (username == null) return false;
+        return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity() {
+        if (password == null || password.length() < 8) return false;
+        boolean hasCapital = false, hasNumber = false, hasSpecial = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) hasCapital = true;
+            else if (Character.isDigit(c)) hasNumber = true;
+            else if (!Character.isLetterOrDigit(c)) hasSpecial = true;
+        }
+        return hasCapital && hasNumber && hasSpecial;
+    }
     
 }
