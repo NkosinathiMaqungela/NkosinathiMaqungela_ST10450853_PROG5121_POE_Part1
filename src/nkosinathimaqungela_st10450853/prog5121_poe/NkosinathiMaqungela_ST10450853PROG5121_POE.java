83c2d9fd28f1fa79d6771b6da3e9081e89986e22
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2026-04-14 09:53:18 +0200

    initialised the code for the project

---
+++ src/nkosinathimaqungela_st10450853/prog5121_poe/NkosinathiMaqungela_ST10450853PROG5121_POE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nkosinathimaqungela_st10450853.prog5121_poe;
+
+/**
+ *
+ * @author maqun
+ */
+public class NkosinathiMaqungela_ST10450853PROG5121_POE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
