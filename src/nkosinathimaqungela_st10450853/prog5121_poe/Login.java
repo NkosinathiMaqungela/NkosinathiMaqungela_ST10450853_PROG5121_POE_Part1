0127a834d5766c74fa621c5e1a14ab11c8b743a2
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2026-04-14 10:59:05 +0200

    completed the functionality for the login

--- src/nkosinathimaqungela_st10450853/prog5121_poe/Login.java
+++ src/nkosinathimaqungela_st10450853/prog5121_poe/Login.java
@@ -9,7 +9,35 @@ public class Login {
     private String username;
     private String password;
     private Registration registration;
-        private boolean loginStatus;
+    private boolean loginStatus;
 
+    public Login(String username, String password, Registration registration) {
+        this.username = username;
+        this.password = password;
+        this.registration = registration;
+    }
+
+    public boolean loginUser() {
+        String storedUsername = registration.getStoredUsername();
+        String storedPassword = registration.getStoredPassword();
+
+        if (storedUsername == null || storedPassword == null) {
+            loginStatus = false;
+            return false;
+        }
+
+        loginStatus = username.equals(storedUsername) && 
+                password.equals(storedPassword);
+        return loginStatus;
+    }
     
+    public String returnLoginStatus() {
+        if (loginStatus) {
+            return "Hello " + registration.getFirstName() + " " +
+                  registration.getLastName() + ", it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
 }
+
